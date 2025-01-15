87e93d8cbc7bd13bdc08cc8eab0a308f4f75a9b1
dhatguy <odunsiolakunbi@gmail.com>
2025-01-15 01:54:10 +0100

    Add social media icons and enhance layout with new components

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    react(),
+    tailwind({ applyBaseStyles: false }),
+    icon({ iconDir: "src/assets/icons" }),
+  ],
 });
