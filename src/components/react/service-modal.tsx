fb8c9df3a8db29b821341b67f793a3278cd1a24f
dhatguy <odunsiolakunbi@gmail.com>
2025-01-21 17:20:41 +0100

    Add utility functions and service modal components with responsive design

---
+++ src/components/react/service-modal.tsx
@@ -0,0 +1,44 @@
+import x from "../../assets/images/x.png";
+
+export default function ServiceModal({ service }: any) {
+  return (
+    <div
+      id={`service-modal-${service.number}`}
+      className="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto hs-overlay-backdrop-open:bg-black/40 hs-overlay-backdrop-open:backdrop-blur-sm"
+      role="dialog"
+      tabIndex={-1}
+      aria-labelledby={`service-modal-${service.number}-label`}
+    >
+      <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all max-w-full max-h-full h-full">
+        <div className="relative flex flex-col-reverse md:flex-row justify-center items-center h-full px-3 md:px-20 gap-6">
+          <div className="text-white">
+            <h2 className="text-4xl font-black">{service.title}</h2>
+            <p>{service.description}</p>
+          </div>
+
+          <div className="relative">
+            <button
+              type="button"
+              className="absolute -right-2 -top-2 size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-red-800 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none"
+              aria-label="Close"
+              data-hs-overlay={`#service-modal-${service.number}`}
+            >
+              <span className="sr-only">Close</span>
+              <img src={x.src} alt="Close" className="w-4 h-4 object-contain" />
+            </button>
+            <img
+              src={service.image.src}
+              alt={service.title}
+              className="rounded-2xl aspect-square md:aspect-video object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// {/* <script>
+//   window.addEventListener('load', () => {
+//   });
+// </script> */}
---
+++ src/components/react/service-section.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+import { cn } from "../../lib/utils";
+import ServiceModal from "./service-modal";
+
+// @ts-expect-error
+const ServicesSection = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const id = setTimeout(() => window.HSOverlay.autoInit(), 1000);
+    return () => clearTimeout(id);
+  }, []);
+
+  return (
+    <section className="p-8 md:p-16 overflow-hidden" id="services">
+      <div className="flex items-center">
+        <div className="max-w-7xl mx-auto flex flex-col gap-12 flex-1">
+          <h2 className="text-3xl font-bold font-euro text-center md:text-left">
+            <span className="text-white">OUR </span>
+            <span className="text-[#40E0D0]">SERVICES</span>
+          </h2>
+          <div className="grid gap-12 items-center">
+            <div className="flex flex-col gap-4">
+              {/* @ts-expect-error */}
+              {items.map((service, index) => (
+                <div
+                  key={service.number}
+                  className="flex flex-col gap-3 cursor-pointer"
+                  data-aos="fade-right"
+                  data-aos-delay={index * 200}
+                >
+                  <div
+                    onClick={() => setActiveIndex(index)}
+                    className={cn(
+                      "flex md:flex-col items-center md:items-start gap-4 md:gap-0 mx-auto md:mx-0 hover:scale-105 transition-all duration-300",
+                      index !== activeIndex && "items-start flex-col mx-0"
+                    )}
+                  >
+                    <span className="text-[#FF3131] text-4xl font-bold font-euro">
+                      {service.number}
+                    </span>
+
+                    <div
+                      className={cn(
+                        "w-0.5 self-center h-8 bg-[#ff9f1c] md:hidden",
+                        index !== activeIndex && "hidden"
+                      )}
+                    />
+
+                    <h3
+                      className={cn(
+                        "text-white text-xl font-bold tracking-wider md:max-w-full font-euro",
+                        index === activeIndex && "max-w-32"
+                      )}
+                    >
+                      {service.title}
+                    </h3>
+                  </div>
+
+                  {index === activeIndex && (
+                    <>
+                      <div
+                        className="rounded-3xl overflow-hidden md:hidden"
+                        aria-haspopup="dialog"
+                        aria-expanded="false"
+                        aria-controls={`service-modal-${service.number}`}
+                        onClick={() =>
+                          window.HSOverlay.open(
+                            `#service-modal-${service.number}`
+                          )
+                        }
+                      >
+                        <img
+                          src={service?.image.src}
+                          alt={service.title}
+                          className="w-full h-full aspect-video object-contain"
+                          loading="eager"
+                        />
+                      </div>
+
+                      {service.description && (
+                        <p className="text-[#8B8B8B]">{service.description}</p>
+                      )}
+                    </>
+                  )}
+                  <ServiceModal service={service} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="relative flex-1 hidden md:block" data-aos="fade-left">
+          <div
+            className="rounded-3xl overflow-hidden cursor-pointer hover:scale-105 transition-all duration-300"
+            aria-haspopup="dialog"
+            aria-expanded="false"
+            aria-controls={`service-modal-${items[activeIndex].number}`}
+            // data-hs-overlay={`#service-modal-${items[activeIndex].number}`}
+            onClick={() =>
+              window.HSOverlay.open(
+                `#service-modal-${items[activeIndex].number}`
+              )
+            }
+          >
+            <img
+              src={items[activeIndex].image.src}
+              alt={items[activeIndex].title}
+              className="w-full h-full aspect-video object-contain"
+              loading="eager"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ServicesSection;
